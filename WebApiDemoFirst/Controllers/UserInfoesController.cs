829a4002f34296aae7b77723e9ff60ef81040ca8
Hridoy Roy <roy.hridoy2014@gmail.com>
2019-10-23 16:56:27 +0600

    Create controller for crud operation and done delete operation

---
+++ WebApiDemoFirst/Controllers/UserInfoesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiDemoFirst.Models;
+
+namespace WebApiDemoFirst.Controllers
+{
+    public class UserInfoesController : ApiController
+    {
+        private APITestEntities db = new APITestEntities();
+
+        // GET: api/UserInfoes
+        public IQueryable<UserInfo> GetUserInfoes()
+        {
+            return db.UserInfoes;
+        }
+
+        // GET: api/UserInfoes/5
+        [ResponseType(typeof(UserInfo))]
+        public IHttpActionResult GetUserInfo(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userInfo);
+        }
+
+        // PUT: api/UserInfoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUserInfo(int id, UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userInfo.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userInfo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/UserInfoes
+        [ResponseType(typeof(UserInfo))]
+        public IHttpActionResult PostUserInfo(UserInfo userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserInfoes.Add(userInfo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = userInfo.id }, userInfo);
+        }
+
+        // DELETE: api/UserInfoes/5
+        [ResponseType(typeof(UserInfo))]
+        public IHttpActionResult DeleteUserInfo(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+
+            return Ok(userInfo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return db.UserInfoes.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ WebApiDemoFirst/Controllers/WebController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDemoFirst.Controllers
+{
+    public class WebController : ApiController
+    {
+        private static List<string> myname = new List<string> { "Hridoy", "Suvo", "Joy", "Bijoy" };
+
+        [HttpGet]
+        public IEnumerable<string> name()
+        {
+            return myname;       
+        }
+
+        [HttpGet]
+        public string namebyid(int id)
+        {
+            return myname[id];
+        }
+    }
+}
